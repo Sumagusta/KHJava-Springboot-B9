5443b2f6c0517081de0ef9680c7125e963229188
ryorangga.suma@gmail.com <ryorangga.suma@gmail.com>
2022-05-23 15:59:58 +0700

    Model

---
+++ javawithapi9/src/main/java/com/kodehive/javawithapi9/model/MahasiswaModel.java
@@ -0,0 +1,11 @@
+package com.kodehive.javawithapi9.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class MahasiswaModel {
+
+	@Id
+	private int mahasiswaId;
+}
